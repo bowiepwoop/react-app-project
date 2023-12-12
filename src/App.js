eaf8723d56bc21273fe9783e559028ab6fd1200d
oda <100525795+bowiepwoop@users.noreply.github.com>
2023-12-12 18:30:04 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import './App.css'
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
 
